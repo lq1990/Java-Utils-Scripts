2d49a16d55caad72f50e371f22222bfee990b4af
liuqiang90 <liuqiang90@foxmail.com>
2020-05-07 11:34:56 +0800

    uml

--- scala_learning/sparkcachepersistcheckpoint/src/main/scala/cn/wendaocp/spark/CachePersist01.scala
+++ scala_learning/sparkcachepersistcheckpoint/src/main/scala/cn/wendaocp/spark/CachePersist01.scala
@@ -27,27 +27,13 @@ import org.apache.spark.{SparkConf, SparkContext}
    * 2. 对一个RDD .cache or .persist之后可以赋值给一个变量，下次直接使用这个变量，就是使用的持久化的rdd
    * 3. 如果赋值给一个变量，那么不要紧跟: res = rdd.cache().collect()
  *
- * 持久化算子：checkpoint
- *   1. 如同复活点。会持久化到磁盘。
- *     当链条上最新的点数据丢失时，会在DAG往前找checkpoint. 而不用一直往前找。
-       因为checkpoint是持久化到磁盘，所以不应该每个rdd都用。
 
-     2. checkpoint 有切断的功能，切断与DAG之前rdd的依赖。
- *
- *   3. 对比 persist(Disk_only): 这个会在当前app运行完了后，磁盘数据清除。
- *     而checkpoint的不会清除，即其他app也可以用。
- *
- *   4. 在某些特定场景，必须用checkpoint。否则报错。
- *
- *   使用checkpoint小技巧：
- *   + 若计划对某个rdd进行 checkpoint，在此之前可以先对rdd cache。这样checkpoint时，就会直接用cache的值，而不用重复计算了
- *
  *
  *
  * @author liuqiang
  * @since 2020/5/1 16:14
  */
-object Cache01 {
+object CachePersist01 {
   def main(args: Array[String]): Unit = {
     println("test cache01")
     val conf = new SparkConf().setMaster("local").setAppName("cacheTest")
