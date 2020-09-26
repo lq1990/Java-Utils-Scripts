74b7e7e836917e606327b2bae5caa3afc119b814
liuqiang <liuqiang90@foxmail.com>
2020-09-26 09:17:24 +0800

    test for, string

--- myutils/src/main/java/stringutils/StringTest.java
+++ myutils/src/main/java/stringutils/StringTest.java
@@ -9,18 +9,33 @@ import java.util.List;
  */
 public class StringTest {
     public static void main(String[] args) {
+        String name = "a.b.c";
+        String replace = name.replace(".", "_");
+        System.out.println(name);
+        System.out.println(replace);
+
         List<Object> list = new ArrayList<Object>();
         list.add("abc1");
         list.add("abc2");
         list.add("abc3");
 
-
+        list.clear();
         for (Object o : list) {
             String str = o.toString();
 
             System.out.println(str);
             System.out.println(str.getClass());
         }
+        System.out.println("1");
+
+        list = null;
+        for (Object o : list) {
+            String str = o.toString();
+
+            System.out.println(str);
+            System.out.println(str.getClass());
+        }
+        System.out.println("2");
 
 
 
