2f6976fab1428a6ffe05df9805cf5abecade8417
liuqiang <liuqiang90@foxmail.com>
2020-10-11 18:06:32 +0800

    hdfs api

--- myutils/src/main/java/hadoop/HDFS01.java
+++ myutils/src/main/java/hadoop/HDFS01.java
@@ -1,13 +1,15 @@
 package hadoop;
 
 import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.fs.FileSystem;
-import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.fs.*;
+import org.apache.hadoop.io.IOUtils;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.io.IOException;
+import java.io.*;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 /**
  * this node has no hadoop.
@@ -22,10 +24,15 @@ public class HDFS01 {
     private static FileSystem fs;
 
     @Before
-    public void before() throws IOException {
-        System.out.println("before");
+    public void before() throws IOException, URISyntaxException {
+        System.out.println("before\n===");
+
         conf = new Configuration(true);
-        fs = FileSystem.get(conf);
+//        fs = FileSystem.get(conf);
+        fs = FileSystem.get(new URI("hdfs://192.168.101.123:8020"), conf);
+
+//        fs = FileSystem.get(new URI("hdfs://ip:port"), conf);
+
     }
 
     @Test
@@ -40,15 +47,76 @@ public class HDFS01 {
     }
 
     @Test
-    public void upload() {
+    public void upload() throws IOException {
+        Path f = new Path("/abcdef/hello.txt"); // target hdfs
+        FSDataOutputStream outputStream = fs.create(f);
 
+        // input stream, input local file
+        InputStream inputStream = new BufferedInputStream(
+                new FileInputStream(new File("/root/data/abc.csv"))); // src local
+
+        // in => out
+        IOUtils.copyBytes(inputStream, outputStream, conf, true);
+
+    }
+
+    @Test
+    public void download() throws IOException {
+        // src hdfs
+        FSDataInputStream inputStream = fs.open(new Path("/abcdef/hello.txt"));
+
+        // target local
+        OutputStream outputStream = new BufferedOutputStream(new FileOutputStream(
+                new File("/root/data/abc2.csv")
+        ));
+
+        // hdfs => local
+        IOUtils.copyBytes(inputStream, outputStream, conf, true);
+
+    }
+
+    /*
+    blocks
+     */
+    @Test
+    public void blks() throws Exception {
+        fs = FileSystem.get(new URI("hdfs://192.168.101.123:8020"), conf);
+
+        FileStatus fileStatus = fs.getFileStatus(new Path("/user/root/abc.csv"));
+//        FileStatus fileStatus = fs.getFileStatus(new Path("/user/root/upload/admin/a.csv"));
+        System.out.println("len: "+fileStatus.getLen());
+
+        BlockLocation[] blks =
+                fs.getFileBlockLocations(
+                        fileStatus, 0, fileStatus.getLen());
+
+        if (blks == null || blks.length == 0) return;
+
+        for (BlockLocation blk : blks) {
+            System.out.println(blk);
+        }
+
+        FSDataInputStream inputStream = fs.open(new Path("/user/root/abc.csv"));
+        System.out.print((char) inputStream.readByte());
+        System.out.print((char) inputStream.readByte());
+        System.out.print((char) inputStream.readByte());
+        System.out.print((char) inputStream.readByte());
+        System.out.print((char) inputStream.readByte());
+        System.out.print((char) inputStream.readByte());
+        System.out.print((char) inputStream.readByte());
+        System.out.print((char) inputStream.readByte());
+        System.out.print((char) inputStream.readByte());
+        System.out.print((char) inputStream.readByte());
+        System.out.print((char) inputStream.readByte());
+        System.out.print((char) inputStream.readByte());
+        System.out.print((char) inputStream.readByte());
 
     }
 
     @After
     public void close() throws IOException {
         fs.close();
-        System.out.println("close");
+        System.out.println("===\nclose");
     }
 
 
